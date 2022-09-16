9024e50972e219a1f3274b22072c77e3b78841e6
Tomjerry12345 <Andrialfiandi46@gmail.com>
2022-09-16 22:41:31 +0800

    test

--- src/config/ApiClient.jsx
+++ src/config/ApiClient.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const apiClient = () => {
   // const apiUrl = "https://jsonplaceholder.typicode.com/";
+  //
 
   const axiosInstance = axios.create({
     baseURL: process.env.BASE_URL,
