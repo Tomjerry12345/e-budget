3a23c1fc1bedff55fa03d31948ef3f1b1c9acbdb
tomjerry <Andrialfiandi46@gmail.com>
2022-12-01 13:12:33 +0800

    v1.3.5

--- src/pages/main/report/laba-rugi/LabaRugiLogic.jsx
+++ src/pages/main/report/laba-rugi/LabaRugiLogic.jsx
@@ -1,17 +1,42 @@
 import { useState } from "react";
 import { columnOutputType1 } from "../../../../component/table/utils/TypeColumn";
 import MainServices from "../../../../services/MainServices";
+import { log } from "../../../../values/Utilitas";
 
 const LabaRugiLogic = () => {
   const [data, setData] = useState();
   const [columns, setColumns] = useState();
   const [loading, setLoading] = useState(false);
 
+  const type = 2;
+
   const onFinish = async (values) => {
     setLoading(true);
     console.log("values", values);
+
     const { code_company, code_dept, code_location, code_product } = values;
-    const { data } = await MainServices.get(`report/labarugi?code_company=${code_company}&code_product=${code_product}&code_location=${code_location}&code_dept=${code_dept}`);
+
+    let url;
+
+    if (type === 1) {
+      url = `report/labarugi?code_company=${code_company}&code_product=${code_product}&code_location=${code_location}&code_dept=${code_dept}`;
+    } else if (type === 2) {
+      let fCodeCompany = code_company.replace(/[^0-9]/g, "");
+      let fCodeProduct = code_product.replace(/[^0-9]/g, "");
+      let fCodeLocation = code_location.replace(/[^0-9]/g, "");
+      let fCodeDept = code_dept.replace(/[^0-9]/g, "");
+
+      // console.log("fCodeCompany", fCodeCompany);
+      // console.log("fCodeProduct", fCodeProduct);
+      // console.log("fCodeLocation", fCodeLocation);
+      // console.log("fCodeDept", fCodeDept);
+
+      url = `report/labarugi?code_company=${fCodeCompany}&code_product=${fCodeProduct}&code_location=${fCodeLocation}&code_dept=${fCodeDept}`;
+    }
+
+    log("url", url);
+
+    const { data } = await MainServices.get(url);
     console.log("res", data);
     getData(data.data);
   };
