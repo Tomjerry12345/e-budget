36d5d9afe7aa898b6f4e52f55b8cdb0f18a2e54d
Tomjerry12345 <andrialfiandi46@gmail.com>
2023-10-23 04:45:13 +0800

    fix: insert, update fasilitas kredit

--- src/pages/main/others/input/fasilitas-kredit/header/HeaderFasilitasKredit.jsx
+++ src/pages/main/others/input/fasilitas-kredit/header/HeaderFasilitasKredit.jsx
@@ -3,13 +3,18 @@ import { Button, Layout, Typography } from "antd";
 const { Header } = Layout;
 const { Text } = Typography;
 
-const HeaderFasilitasKredit = ({ titleHeader = "", onOpenModal }) => {
+const HeaderFasilitasKredit = ({ titleHeader = "", onOpenModal, disabled }) => {
   return (
     <Header className="custom-header">
       <Text className="header-title">{titleHeader}</Text>
 
       <div className="container-menu">
-        <Button className="btn-tambah-user" onClick={onOpenModal} type="primary">
+        <Button
+          className="btn-tambah-user"
+          onClick={onOpenModal}
+          type="primary"
+          disabled={disabled}
+        >
           Tambah Fasilitas Kredit
         </Button>
       </div>
--- src/pages/main/others/input/fasilitas-kredit/modal/ModalFasilitasKredit.jsx
+++ src/pages/main/others/input/fasilitas-kredit/modal/ModalFasilitasKredit.jsx
@@ -1,7 +1,10 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Button, Form, Input, Typography } from "antd";
+import { Button, Form, Input, Select, Typography } from "antd";
 import Modal from "antd/lib/modal/Modal";
 import "./style.scss";
+import MainServices from "services/MainServices";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const { Title } = Typography;
 
@@ -21,21 +24,120 @@ const FormItem = ({ label, name, children }) => (
 );
 
 const ModalFasilitasKredit = ({ open, onCancel, onOk, form }) => {
+  const [listBank, setListBank] = useState([]);
+  const [listAccount, setListAccount] = useState([]);
+  const [params, setParams] = useState({
+    nama_bank: "",
+    type: "",
+    account: "",
+  });
+
+  const listType = [
+    {
+      value: "long",
+      label: "Long",
+    },
+    {
+      value: "short",
+      label: "Short",
+    },
+  ];
+
+  useEffect(() => {
+    getListNamaBank();
+  }, []);
+
+  useEffect(() => {
+    if (params.nama_bank !== "" && params.type !== "") {
+      getListAccount();
+    }
+  }, [params]);
+
+  const getListNamaBank = async () => {
+    try {
+      const { data } = await MainServices.get("config/credit-facility/names");
+
+      setListBank(data.data);
+    } catch (e) {}
+  };
+
+  const getListAccount = async () => {
+    try {
+      const { data } = await MainServices.get(
+        `config/credit-facility/accounts/${params.nama_bank}?type=${params.type}`
+      );
+
+      setListAccount(data.data);
+    } catch (e) {}
+  };
+
   return (
     <Modal className="management-user" open={open} footer={null} onCancel={onCancel}>
       <Title level={4}>Tambah Fasilitas Kredit</Title>
       <Form onFinish={onOk} layout="vertical" form={form} autoComplete="off">
-        <FormItem label="NIK" name="nik" children={<Input />} />
+        <FormItem
+          label="Nama Bank"
+          name="nama_bank"
+          children={
+            <Select
+              allowClear
+              onChange={(e) => {
+                setParams({
+                  ...params,
+                  nama_bank: e,
+                });
+                form.setFieldsValue({ nama_bank: e });
+              }}
+              options={listBank.map((e) => ({
+                value: e.slug,
+                label: e.name,
+              }))}
+            />
+          }
+        />
+        <FormItem
+          label="Type"
+          name="type"
+          children={
+            <Select
+              allowClear
+              onChange={(e) => {
+                setParams({
+                  ...params,
+                  type: e,
+                });
+                form.setFieldsValue({ type: e });
+              }}
+              options={listType}
+            />
+          }
+        />
+        <FormItem
+          label="Code Account"
+          name="code_account"
+          children={
+            <Select
+              allowClear
+              onChange={(e) => {
+                setParams({
+                  ...params,
+                  code_account: e,
+                });
+                form.setFieldsValue({ code_account: e });
+              }}
+              options={listAccount.map((e) => ({
+                value: e.account,
+                label: e.description,
+              }))}
+            />
+          }
+        />
 
         <Form.Item className="footer-custom">
           <Button className="btn-cancel" type="text" onClick={onCancel}>
             Cancel
           </Button>
-          <Button
-            className="btn-tambah"
-            type="primary"
-            htmlType="submit"
-          >
+          <Button className="btn-tambah" type="primary" htmlType="submit">
             Tambah
           </Button>
         </Form.Item>
