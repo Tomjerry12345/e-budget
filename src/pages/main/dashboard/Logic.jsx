9bc2b7d9b326159de70257e2625526d3c96fe01e
Tomjerry12345 <andrialfiandi46@gmail.com>
2023-11-05 05:54:51 +0800

    fix: 05/11/2023 5:54
    new: dashboard

--- src/pages/main/dashboard/Logic.jsx
+++ src/pages/main/dashboard/Logic.jsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react";
+import MainServices from "services/MainServices";
+import { log } from "values/Utilitas";
+
 const Logic = () => {
   const dataSource = [
     {
@@ -19,6 +23,10 @@ const Logic = () => {
       title: "No",
       dataIndex: "no",
       key: "no",
+      render: (id, record, index) => {
+        ++index;
+        return index;
+      },
     },
     {
       title: "SBU",
@@ -27,15 +35,92 @@ const Logic = () => {
     },
     {
       title: "Revenue",
-      dataIndex: "revenue",
-      key: "revenue",
+      dataIndex: "value",
+      key: "value",
     },
   ];
+
+  const [dataMain, setDataMain] = useState();
+  const [dataGrowth, setDataGrowth] = useState([]);
+  const [dataTopRevenue, setDataTopRevenue] = useState([]);
+  const [dataTopEbt, setDataTopEbt] = useState([]);
+
+  useEffect(() => {
+    onGetData();
+  }, []);
+
+  const onGetData = async () => {
+    const url = `dashboard`;
+    try {
+      const { data } = await MainServices.get(url);
+      const d = data.data;
+      onFormatMainChart(d);
+      onFormatGrowthChart(d);
+      onTopRevenue(d);
+      onTopEbt(d);
+    } catch (error) {
+      console.error(`Error fetching data for code account`, error);
+    }
+  };
+
+  const onFormatMainChart = (data) => {
+    const mainChart = data.main_chart;
+    const mainFixChart = {
+      labels: mainChart.labels,
+      datasets: [
+        {
+          data: mainChart.datasets.data,
+          backgroundColor: mainChart.labels.map((e, i) => {
+            return i % 2 === 0
+              ? mainChart.datasets.background1
+              : mainChart.datasets.background2;
+          }),
+        },
+      ],
+    };
+    setDataMain(mainFixChart);
+  };
+
+  const onFormatGrowthChart = (data) => {
+    const listGrowthFixChart = [];
+    const growthChart = data.growth_chart;
+    growthChart.forEach((e, i) => {
+      const growthFixChart = {
+        labels: e.labels,
+        datasets: [
+          {
+            data: e.datasets.data,
+            backgroundColor: e.labels.map((j, i) => {
+              return i % 2 === 0 ? e.datasets.background1 : e.datasets.background2;
+            }),
+          },
+        ],
+      };
+
+      listGrowthFixChart.push(growthFixChart);
+    });
+
+    log({ listGrowthFixChart });
+
+    setDataGrowth(listGrowthFixChart);
+  };
+
+  const onTopRevenue = (data) => {
+    setDataTopRevenue(data.top_revenue);
+  };
+
+  const onTopEbt = (data) => {
+    setDataTopEbt(data.top_ebt);
+  };
 
   return {
     value: {
       columns,
       dataSource,
+      dataMain,
+      dataGrowth,
+      dataTopRevenue,
+      dataTopEbt,
     },
     func: {},
   };
